dd33985651b5368281b88edadd3e95e819e44f10
L3onel-o <00145424@uca.edu.sv>
2026-03-19 20:22:39 -0600

    Initial commit

---
+++ Labo1/src/Eje1.kt
@@ -0,0 +1,18 @@
+class Computadora(var ram: Int, var memoria: Int, var sistemaOperativo: String, val tipoComputadora: String){
+
+    //Funcion de encendido
+    fun encender(){
+        println("La computadora ha sido encendida")
+    }
+
+    //Funcion para apagarla
+    fun apagar(){
+        println("La computadora ha sido apagada")
+    }
+
+    var listaApps = listof("")
+}
+
+fun main() {
+
+}
